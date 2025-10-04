b07aa62b5063e2f46a68e705de54a192763c1926
culinarystarfleet <culinarystarfleet@proton.me>
2025-10-04 16:54:16 +0200

    Update lang_switch.js

--- assets/js/lang_switch.js
+++ assets/js/lang_switch.js
@@ -6,6 +6,4 @@ function switchLang(lang) {
 document.addEventListener('DOMContentLoaded', () => {
   const lang = localStorage.getItem('lang') || 'de';
   document.documentElement.lang = lang;
-
-  // TODO: später die Kategorien und Rezepte dynamisch nach Sprache wechseln
 });
